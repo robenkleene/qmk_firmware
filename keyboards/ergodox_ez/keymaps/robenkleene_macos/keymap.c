81be310e19ce79ed124a59b1f16701c8938d3aef
Roben Kleene <contact@robenkleene.com>
2019-02-25 11:51:44 -0500

    Update map

--- keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
+++ keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
@@ -10,18 +10,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Default
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  |      |           |  +   |   6  |   7  |   8  |   9  |   0  |   -    |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |  BS    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCTL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   "    |
- * |--------+------+------+------+------+------| LCTL |           | RCTL |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   ?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  \   |   ~  |LCTRL | LALT | LGUI |                                       | RGUI | RALT |   {  |   }  | Fn   |
+ *   |  Fn  |   ~  |LCTRL | LALT | LGUI |                                       | RGUI | RALT |   {  |   }  | Fn   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | CTRL |  Fn  |       |  Fn  | CTRL |
+ *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 |Space | RET  |------|       |------|  Ret   |Space |
@@ -34,26 +34,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
 
 // Left Keyboard
-KC_ESC,   KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_TRNS,
-KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,  KC_RBRC,
-KC_LCTL,  KC_A,    KC_S,     KC_D,     KC_F,     KC_G,
-KC_LSFT,  KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,  KC_LCTL,
-KC_BSLS,  KC_GRV,  KC_LCTL,  KC_LALT,  KC_LGUI,
+KC_ESC,    KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_NO,
+KC_TAB,    KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,  KC_NO,
+KC_LCTL,   KC_A,    KC_S,     KC_D,     KC_F,     KC_G,
+KC_LSFT,   KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,  KC_NO,
+MO(FCTN),  KC_GRV,  KC_LCTL,  KC_LALT,  KC_LGUI,
 
 // Left Cluster
-KC_LCTL,  MO(FCTN),
+KC_NO,    KC_NO,
 KC_HOME,
-KC_SPC,   KC_ENT,    KC_END,
+KC_SPC,   KC_ENT,  KC_END,
 
 // Right Keyboard
-KC_EQL,   KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,
+KC_NO,    KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,
 KC_NO,    KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_BSPC,
 KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,
 KC_RCTL,  KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,
 KC_RGUI,  KC_RALT,  KC_LBRC,  KC_RBRC,  MO(FCTN),
 
 // Right Cluster
-MO(FCTN), KC_RCTL,
+KC_NO,    KC_NO,
 KC_PGUP,
 KC_PGDN,  KC_ENT,  KC_SPC
 
@@ -62,9 +62,9 @@ KC_PGDN,  KC_ENT,  KC_SPC
 /* Function
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | TNSY   |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+ * | TNSY   |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   +    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           | VolU |  FF  | Mute |      |      |      |        |
+ * |        |      |      |      |      |      |      |           | VolU |  FF  | Mute |      |      |      |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |------|           |------| Play |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           | VolD |------+------+------+------+------+--------|
@@ -81,7 +81,6 @@ KC_PGDN,  KC_ENT,  KC_SPC
  *                                 `--------------------'       `--------------------'
  */
 
-
 // SYMBOLS
 [FCTN] = LAYOUT_ergodox(
 
@@ -97,8 +96,8 @@ KC_TRNS,
 KC_TRNS,  KC_TRNS,  KC_TRNS,
 
 // Right Keyboard
-KC_TRNS,  KC_F6,    KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_F11,
-KC_VOLU,  KC_MNXT,  KC_MUTE,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_F6,    KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_EQL,
+KC_VOLU,  KC_MNXT,  KC_MUTE,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_BSLS,
 KC_MPLY,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
 KC_VOLD,  KC_MPRV,  KC_TRNS,  KC_TRNS,   KC_UP,    KC_TRNS,  KC_TRNS,
 KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_TRNS,
