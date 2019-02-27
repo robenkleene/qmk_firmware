2bb05d83dd7ee666f5c401b2688e13b2d6b04062
Roben Kleene <contact@robenkleene.com>
2019-02-27 11:25:00 -0500

    Update map

--- keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
+++ keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Default
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  |  =   |           |  =   |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |  BS    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   ?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  Fn  |   ~  |LCTRL | LALT | LGUI |                                       | RGUI | RALT |   {  |   }  | Fn   |
+ *   |  Fn  |   ~  |   \  | LALT | LGUI |                                       | RGUI | RALT |   {  |   }  | Fn   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -34,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
 
 // Left Keyboard
-KC_ESC,    KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_NO,
+KC_ESC,    KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_EQL,
 KC_TAB,    KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,  KC_NO,
 KC_LCTL,   KC_A,    KC_S,     KC_D,     KC_F,     KC_G,
 KC_LSFT,   KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,  KC_NO,
-MO(FCTN),  KC_GRV,  KC_LCTL,  KC_LALT,  KC_LGUI,
+MO(FCTN),  KC_GRV,  KC_BSLS,  KC_LALT,  KC_LGUI,
 
 // Left Cluster
 KC_NO,    MO(FCTN),
@@ -46,7 +46,7 @@ KC_HOME,
 KC_SPC,   KC_ENT,  KC_END,
 
 // Right Keyboard
-KC_NO,    KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,
+KC_EQL,   KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,
 KC_NO,    KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_BSPC,
 KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,
 KC_RCTL,  KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,
@@ -62,7 +62,7 @@ KC_PGDN,   KC_ENT,  KC_SPC
 /* Function
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | TNSY   |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   +    |
+ * | TNSY   |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   +    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           | VolU |  FF  | Mute |      |      |      |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -97,7 +97,7 @@ KC_TRNS,  KC_TRNS,  KC_TRNS,
 
 // Right Keyboard
 KC_TRNS,  KC_F6,    KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_EQL,
-KC_VOLU,  KC_MNXT,  KC_MUTE,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_BSLS,
+KC_VOLU,  KC_MNXT,  KC_MUTE,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,
 KC_MPLY,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
 KC_VOLD,  KC_MPRV,  KC_TRNS,  KC_TRNS,   KC_UP,    KC_TRNS,  KC_TRNS,
 KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_TRNS,
