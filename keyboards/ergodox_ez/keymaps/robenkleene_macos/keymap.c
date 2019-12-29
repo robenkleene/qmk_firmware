003f3206f4cb000624c189c12d1708ed6b78729a
Roben Kleene <contact@robenkleene.com>
2019-12-29 13:41:10 -0500

    Update based on Planck

--- keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
+++ keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
@@ -66,9 +66,9 @@ KC_PGDN,   KC_ENT,   KC_SPC
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |   `    |   1  |   2  |   3  |   4  |   5  | BriU |           | VolU |   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|           |------| Play |  -   |  =   |  [   |  ]   |   \    |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |------|           |------|  F6  |  -   |  =   |  [   |  ]   |   \    |
  * |--------+------+------+------+------+------| BriD |           | VolD |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      | PgUp | HOME |  UP  | END  |        |
+ * |        |  F7  |  F8  |  F9  |  F10 | F11  |      |           |      | F12  | PgUp | HOME |  UP  | END  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      | LCTRL| LALT | LGUI |                                       | PgDn | LEFT | DOWN |RIGHT |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -76,7 +76,7 @@ KC_PGDN,   KC_ENT,   KC_SPC
  *                                        |      |      |       |  RW  | FF   |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 | Play |      |------|       |------|      | Play |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -85,27 +85,27 @@ KC_PGDN,   KC_ENT,   KC_SPC
 [FCTN] = LAYOUT_ergodox(
 
 // Left Keyboard
-RESET,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_TRNS,
-KC_GRV,   KC_1,     KC_2,     KC_3,     KC_3,     KC_4,     KC_5,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_BRID,
+RESET,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_TRNS,
+KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,    KC_BRIU,
+KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
+KC_TRNS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_BRID,
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 // Left Cluster
 KC_TRNS,  KC_TRNS,
 KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_MPLY,  KC_TRNS,  KC_TRNS,
 
 // Right Keyboard
 KC_TRNS,  KC_F6,    KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_EQL,
 KC_VOLU,  KC_6,     KC_7,     KC_8,      KC_9,     KC_0,     KC_TRNS,
-KC_MPLY,  KC_MINS,  KC_EQL,   KC_LBRC,   KC_RBRC,  KC_BSLS,
-KC_VOLD,  KC_TRNS,  KC_PGUP,  KC_HOME,   KC_UP,    KC_END,   KC_TRNS,
+KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,   KC_RBRC,  KC_BSLS,
+KC_VOLD,  KC_F12,   KC_PGUP,  KC_HOME,   KC_UP,    KC_END,   KC_TRNS,
 KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_TRNS,
 
 // Right Cluster
 KC_MPRV,  KC_MNXT,
 KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS
+KC_TRNS,  KC_TRNS,  KC_MPLY
 
 ),
 
