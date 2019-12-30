c1111e28f1e8abb986b8acce79b7597efeca2422
Roben Kleene <contact@robenkleene.com>
2019-12-30 12:28:55 -0500

    Add return

--- keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
+++ keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
@@ -18,9 +18,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |  -   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   ?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Ret  | LCTRL| LALT | LGUI |  Fn  |                                       |  Fn  | RGUI | RALT | RCTRL| Ret  |
+ *   | Ret  | LCTRL| LALT | LGUI |  Fn  |                                       |  Fn  | RGUI | RALT | RCTRL|  Ret |
  *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
+                                          ,-------------.       ,-------------.
  *                                        | LGUI |  Fn  |       |  Fn  | RGUI |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
@@ -64,19 +64,19 @@ KC_PGDN,   KC_ENT,   KC_SPC
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |   `    |   1  |   2  |   3  |   4  |   5  | BriU |           | VolU |   6  |   7  |   8  |   9  |   0  |        |
+ * |   `    |   1  |   2  |   3  |   4  |   5  | BriU |           | VolU |   6  |   7  |   8  |   9  |   0  |  TNSY  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |------|           |------|  F6  |  -   |  =   |  [   |  ]   |   \    |
  * |--------+------+------+------+------+------| BriD |           | VolD |------+------+------+------+------+--------|
  * |        |  F7  |  F8  |  F9  |  F10 | F11  |      |           |      | F12  | Play | HOME |  UP  | END  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *w  |      |      |      |      | TNSY |                                       |      | LEFT | DOWN |RIGHT |      |
+ *w  |      |      |      |      |      |                                       |      | LEFT | DOWN |RIGHT |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |  RW  | FF   |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 | Ret  |      |------|       |------|      | Ret  |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -89,15 +89,15 @@ KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_TRNS,
 KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,    KC_BRIU,
 KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
 KC_TRNS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_BRID,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RESET,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 // Left Cluster
 KC_TRNS,  KC_TRNS,
 KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_ENT,   KC_TRNS,  KC_TRNS,
 
 // Right Keyboard
-KC_TRNS,  KC_F6,    KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_EQL,
-KC_VOLU,  KC_6,     KC_7,     KC_8,      KC_9,     KC_0,     KC_TRNS,
+KC_TRNS,  KC_F6,    KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_TRNS,
+KC_VOLU,  KC_6,     KC_7,     KC_8,      KC_9,     KC_0,     RESET,
 KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,   KC_RBRC,  KC_BSLS,
 KC_VOLD,  KC_F12,   KC_MPLY,  KC_HOME,   KC_UP,    KC_END,   KC_TRNS,
 KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_TRNS,
@@ -105,7 +105,7 @@ KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_TRNS,
 // Right Cluster
 KC_MPRV,  KC_MNXT,
 KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS
+KC_TRNS,  KC_TRNS,  KC_ENT
 
 ),
 
