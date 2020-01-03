4fbed55b0d9a1bf68ef8de6153dcc442c02d94e1
Roben Kleene <contact@robenkleene.com>
2020-01-03 12:12:01 -0500

    Revert "Remove function keys"
    This reverts commit c304f6662574f7978a4d783d75b943aa0d31170b.

--- keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
+++ keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
@@ -64,15 +64,15 @@ KC_PGDN,   KC_NO,    KC_SPC
 /* Function
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  | TNSY |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |   `    |   1  |   2  |   3  |   4  |   5  | BriU |           | VolU |   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|           |------|      |  -   |  =   |  [   |  ]   |   \    |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |------|           |------|  F6  |  -   |  =   |  [   |  ]   |   \    |
  * |--------+------+------+------+------+------| BriD |           | VolD |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      | Play | HOME |  UP  | END  |       |
+ * |        |  F7  |  F8  |  F9  |  F10 | F11  |      |           |      | F12  | Play | HOME |  UP  | END  |       |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *w  |      |      |      |      | TNSY |                                       |      | LEFT | DOWN |RIGHT |      |
+ *w  | TNSY |      |      |      |      |                                       |      | LEFT | DOWN |RIGHT |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |  RW  | FF   |
@@ -87,10 +87,10 @@ KC_PGDN,   KC_NO,    KC_SPC
 [FCTN] = LAYOUT_ergodox(
 
 // Left Keyboard
-KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_TRNS,
-KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_BRIU,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_BRID,
+KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_TRNS,
+KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,    KC_BRIU,
+KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
+KC_TRNS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_BRID,
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RESET,
 
 // Left Cluster
@@ -101,8 +101,8 @@ KC_TRNS,  KC_TRNS,  KC_TRNS,
 // Right Keyboard
 KC_TRNS,  KC_F6,    KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_TRNS,
 KC_VOLU,  KC_6,     KC_7,     KC_8,      KC_9,     KC_0,     KC_TRNS,
-KC_TRNS,  KC_MINS,  KC_EQL,   KC_LBRC,   KC_RBRC,  KC_BSLS,
-KC_VOLD,  KC_TRNS,  KC_MPLY,  KC_HOME,   KC_UP,    KC_END,   KC_TRNS,
+KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,   KC_RBRC,  KC_BSLS,
+KC_VOLD,  KC_F12,   KC_MPLY,  KC_HOME,   KC_UP,    KC_END,   KC_TRNS,
 KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_TRNS,
 
 // Right Cluster
