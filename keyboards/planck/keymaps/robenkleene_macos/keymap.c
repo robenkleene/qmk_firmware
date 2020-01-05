44e623fd389cb88e33805e8f89b356962d5c60e0
Roben Kleene <contact@robenkleene.com>
2020-01-05 15:25:03 -0500

    Make first layer

--- keyboards/planck/keymaps/robenkleene_macos/keymap.c
+++ keyboards/planck/keymaps/robenkleene_macos/keymap.c
@@ -40,10 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_planck_grid(
-KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC,
-KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,   KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT    ,
-KC_TAB,   KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LGUI,  KC_SPC,  KC_SPC,  KC_TAB,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT
+KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,     KC_O,     KC_P,     KC_BSPC,
+KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,  KC_G,    KC_H,    KC_J,  KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+ARRW,     KC_LCTL,  KC_LALT,  KC_LGUI,  FCTN,  KC_SPC,  KC_SPC,  FCTN,  KC_RGUI,  KC_RALT,  KC_RCTL,  KC_ENT
 ),
 
 };
