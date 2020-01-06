3d4d77825af7995f3412a5ddbff3d060b5a9f2fd
Roben Kleene <contact@robenkleene.com>
2020-01-06 10:04:27 -0500

    Swap ESC and tab

--- keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
+++ keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Left Keyboard
 KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,      KC_5,  KC_NO,
-KC_ESC,    KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,  KC_NO,
-KC_TAB,    KC_A,     KC_S,     KC_D,     KC_F,      KC_G,
+KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,  KC_NO,
+KC_ESC,    KC_A,     KC_S,     KC_D,     KC_F,      KC_G,
 KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,  KC_NO,
 TT(ARRW),  KC_LCTL,  KC_LALT,  KC_LGUI,  MO(FCTN),
 
