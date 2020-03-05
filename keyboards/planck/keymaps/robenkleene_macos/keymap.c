225973fd27499ab561e2b12fe7fe40e14a001654
Roben Kleene <contact@robenkleene.com>
2020-03-05 18:28:05 -0500

    Add page keys

--- keyboards/planck/keymaps/robenkleene_macos/keymap.c
+++ keyboards/planck/keymaps/robenkleene_macos/keymap.c
@@ -20,10 +20,12 @@
 enum planck_layers {
   _BASE,
   _FCTN,
+  _FCT2,
   _UTIL
 };
 
 #define FCTN MO(_FCTN)
+#define FCT2 MO(_FCT2)
 #define UTIL MO(_UTIL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -36,10 +38,17 @@ TG(_FCTN),  KC_LCTL,  KC_LALT,  KC_LGUI,  FCTN,  KC_SPC,  KC_SPC,  FCTN,  KC_RGU
 ),
 
 [_FCTN] = LAYOUT_planck_grid(
-KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_TRNS,
-UTIL,     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
-KC_TRNS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MPLY,  KC_VOLD,  KC_VOLU,  KC_UP,    KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT
+KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,    KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_TRNS,
+UTIL,     KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,    KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
+KC_TRNS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,   KC_F12,   KC_MPLY,  KC_VOLD,  KC_VOLU,  KC_UP,    KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  FCT2,    KC_TRNS,  KC_TRNS,  FCT2,     KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT
+),
+
+[_FCT2] = LAYOUT_planck_grid(
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGUP,  KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_HOME,  KC_PGDN,  KC_END
 ),
 
 [_UTIL] = LAYOUT_planck_grid(
@@ -65,6 +74,9 @@ void rgb_matrix_indicators_user(void) {
       break;
     case _FCTN:
       rgb_matrix_set_color_all(25, 0, 0);
+      break;
+    case _FCT2:
+      rgb_matrix_set_color_all(0, 0, 25);
       break;
     case _UTIL:
       rgb_matrix_set_color_all(0, 25, 0);
