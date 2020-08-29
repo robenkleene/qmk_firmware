3781c09dbc66da9a78b97c502b16368ae2b8cf41
Roben Kleene <contact@robenkleene.com>
2020-08-29 15:29:55 -0400

    Tweak number row

--- keyboards/planck/keymaps/robenkleene_macos/keymap.c
+++ keyboards/planck/keymaps/robenkleene_macos/keymap.c
@@ -33,10 +33,10 @@ enum planck_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT_planck_grid(
-KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,     KC_O,     KC_P,     KC_BSPC,
-KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,  KC_G,    KC_H,    KC_J,  KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-NUMB,     KC_LCTL,  KC_LALT,  KC_LGUI,  FCTN,  KC_SPC,  KC_SPC,  FCTN,  KC_RGUI,  KC_RALT,  KC_RCTL,  KC_ENT
+KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,     KC_O,     KC_P,     KC_BSPC,
+KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,  KC_G,    KC_H,    KC_J,  KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+KC_LSFT,    KC_Z,     KC_X,     KC_C,     KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+TG(_NUMB),  KC_LCTL,  KC_LALT,  KC_LGUI,  FCTN,  KC_SPC,  KC_SPC,  FCTN,  KC_RGUI,  KC_RALT,  KC_RCTL,  KC_ENT
 ),
 
 [_FCTN] = LAYOUT_planck_grid(
@@ -82,10 +82,10 @@ void rgb_matrix_indicators_user(void) {
       rgb_matrix_set_color_all(17, 25, 17);
       break;
     case _FCTN:
-      rgb_matrix_set_color_all(0, 0, 25);
+      rgb_matrix_set_color_all(17, 17, 25);
       break;
     case _FCT2:
-      rgb_matrix_set_color_all(17, 17, 25);
+      rgb_matrix_set_color_all(0, 0, 25);
       break;
     case _NUMB:
       rgb_matrix_set_color_all(0, 25, 0);
