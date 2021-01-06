3c5b9bb72dbe1855a045a45d828326b4a1ce5e73
Roben Kleene <contact@robenkleene.com>
2021-01-06 11:53:56 -0500

    Use replace rather than layer

--- keyboards/planck/keymaps/robenkleene_macos/keymap.c
+++ keyboards/planck/keymaps/robenkleene_macos/keymap.c
@@ -43,7 +43,7 @@ TG(_FCTN),  KC_LCTL,  KC_LALT,  KC_LGUI,  FCTN,  KC_SPC,  KC_SPC,  FCTN,  KC_RGU
 KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,    KC_5,     KC_6,     KC_7,     KC_8,       KC_9,     KC_0,     KC_TRNS,
 UTIL,     KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,    KC_F6,    KC_MINS,  KC_EQL,     KC_LBRC,  KC_RBRC,  KC_BSLS,
 KC_TRNS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,   KC_F12,   KC_MPLY,  KC_VOLD,    KC_VOLU,  KC_UP,    KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  FCT2,    KC_TRNS,  KC_TRNS,  FCT2,     TG(_NUMB),  KC_LEFT,  KC_DOWN,  KC_RIGHT
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  FCT2,    KC_TRNS,  KC_TRNS,  FCT2,     TO(_NUMB),  KC_LEFT,  KC_DOWN,  KC_RIGHT
 ),
 
 [_FCT2] = LAYOUT_planck_grid(
