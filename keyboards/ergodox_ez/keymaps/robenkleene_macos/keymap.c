b53c2f6623ad419e52d6925221474978e962002a
Roben Kleene <contact@robenkleene.com>
2021-02-04 11:20:51 -0500

    Switch back to numb key in bottom left

--- keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
+++ keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
@@ -41,7 +41,7 @@ KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,      KC_5,  KC_NO,
 KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,  KC_NO,
 KC_ESC,    KC_A,     KC_S,     KC_D,     KC_F,      KC_G,
 KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,  KC_NO,
-TG(FCTN),  KC_LCTL,  KC_LALT,  KC_LGUI,  MO(FCTN),
+TG(NUMB),  KC_LCTL,  KC_LALT,  KC_LGUI,  MO(FCTN),
 
 // Left Cluster
 KC_LGUI,  MO(FCTN),
@@ -88,11 +88,11 @@ KC_PGDN,   KC_ENT,   KC_SPC
 [FCTN] = LAYOUT_ergodox(
 
 // Left Keyboard
-KC_TRNS,   KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,   KC_TRNS,
-KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,      KC_5,    KC_BRIU,
-MO(UTIL),  KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,
-KC_TRNS,   KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,  KC_BRID,
-KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  MO(FCT2),
+KC_TRNS,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_TRNS,
+KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,    KC_BRIU,
+MO(UTIL),  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
+KC_TRNS,   KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_BRID,
+MO(FCT2),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 
 // Left Cluster
 KC_TRNS,  KC_TRNS,
@@ -100,11 +100,11 @@ KC_TRNS,
 KC_TRNS,  KC_TRNS,  KC_TRNS,
 
 // Right Keyboard
-KC_TRNS,   KC_F6,     KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_TRNS,
-KC_VOLU,   KC_6,      KC_7,     KC_8,     KC_9,      KC_0,     KC_TRNS,
-KC_F6,     KC_MINS,   KC_EQL,   KC_LBRC,  KC_RBRC,   KC_BSLS,
-KC_VOLD,   KC_F12,    KC_MPLY,  KC_VOLD,  KC_VOLU,   KC_UP,    KC_TRNS,
-MO(FCT2),  TO(NUMB),  KC_LEFT,  KC_DOWN,  KC_RIGHT,
+KC_TRNS,  KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_TRNS,
+KC_VOLU,  KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_TRNS,
+KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_BSLS,
+KC_VOLD,  KC_F12,   KC_MPLY,  KC_VOLD,  KC_VOLU,   KC_UP,    KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,
 
 // Right Cluster
 KC_MPRV,  KC_MNXT,
@@ -132,7 +132,7 @@ KC_TRNS,  KC_TRNS,  KC_TRNS,
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPRV,  KC_MNXT,   KC_PGUP,  KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_MUTE,  KC_MPRV,  KC_MNXT,   KC_PGUP,  KC_TRNS,
 KC_TRNS,  KC_TRNS,  KC_HOME,  KC_PGDN,  KC_END,
 
 // Right Cluster
